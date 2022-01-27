342b447b6728d96a5424f8b42345c8d1e33af168
Freddi <mail@FrederikHeinrich.de>
2022-01-27 23:23:02 +0100

    add Firebase

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBI2evP8x0SZH1JQJ6juRmV3wQnIaRyFxQ",
+  authDomain: "freddi-network.firebaseapp.com",
+  databaseURL: "https://freddi-network-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "freddi-network",
+  storageBucket: "freddi-network.appspot.com",
+  messagingSenderId: "534966381727",
+  appId: "1:534966381727:web:d3f5dc92cd1a8cdae0c6aa",
+  measurementId: "G-RJCQR2SZX7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
